23cbed1e340e89cb6de7cc88853d54b2c3073b4d
zyzle <colin.mcculloch@komi.io>
2023-02-23 13:41:41 +0000

    feat(analytics): add vercel analytics
    giving the vercel analytics a try, may use instead of microanalytics if it gives me everything i
    need have them side by side for now

--- components/Analytics.tsx
+++ components/Analytics.tsx
@@ -1,10 +1,12 @@
 'use client';
 
 import Script from 'next/script';
+import { Analytics as VercelAnalytics } from '@vercel/analytics/react';
 
 export default function Analytics() {
 	return (
 		<>
+			<VercelAnalytics />
 			<Script
 				data-host="https://microanalytics.io"
 				data-dnt="false"
