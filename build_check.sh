c5d8905493e0c70bdf4f075bbba5acda630f7ab0
zyzle <colin.mcculloch@komi.io>
2023-02-24 19:53:54 +0000

    ci(check): add tag check to build_check script

--- build_check.sh
+++ build_check.sh
@@ -4,6 +4,14 @@ echo "VERCEL_GIT_COMMIT_REF: $VERCEL_GIT_COMMIT_REF"
 
 if [[ "$VERCEL_GIT_COMMIT_REF" == "master"  ]] ; then
   # Dont build master, we trigger it only with releases
+  git describe --exact-match --tags HEAD 2> /dev/null
+
+  if [ $? -eq 0 ]; then
+    # Build if we have a tag
+    echo "✅ - Build can proceed"
+    exit 1;
+  fi
+
   echo "🛑 - Master build, cancelled"
   exit 0;
 
