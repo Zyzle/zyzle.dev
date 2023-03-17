8a6f4c3eaedf6544bf9649e9c71e08efda6d48df
zyzle <colin.mcculloch@komi.io>
2023-03-17 01:37:41 +0000

    refactor(analytics): remove microanalytics
    since microanalytics no longer offer a free tier and vercel gives mostly the same info I've moved
    analytics over there

--- components/Analytics.tsx
+++ components/Analytics.tsx
@@ -1,18 +1,10 @@
 'use client';
-
-import Script from 'next/script';
 import { Analytics as VercelAnalytics } from '@vercel/analytics/react';
 
 export default function Analytics() {
 	return (
 		<>
 			<VercelAnalytics />
-			<Script
-				data-host="https://microanalytics.io"
-				data-dnt="false"
-				src="https://microanalytics.io/js/script.js"
-				id="ZwSg9rf6GA"
-			/>
 		</>
 	);
 }
