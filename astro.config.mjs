33c4d6033727dcac0b108b66f70f8121739fe64b
Colin McCulloch <colin.mcculloch@whocanfixmycar.com>
2025-08-25 22:29:46 +0100

    feat: basic pages added

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,25 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+
+import tailwindcss from "@tailwindcss/vite";
+
+import expressiveCode from "astro-expressive-code";
+
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()],
+  },
+
+  integrations: [
+    expressiveCode({
+      themes: ["one-dark-pro"],
+      styleOverrides: {
+        codeFontFamily: "'Fira Code Variable', monospace",
+      },
+    }),
+    mdx(),
+  ],
+});
