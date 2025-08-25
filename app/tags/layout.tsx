579c0e26fe2fab16ed154ff8c94d923c07020b9b
Colin McCulloch <colin.mcculloch@whocanfixmycar.com>
2025-08-25 12:35:17 +0100

    chore: cleanup old files

--- app/tags/layout.tsx
+++
@@ -1,3 +0,0 @@
-export default function TagsLayout({ children }: { children: React.ReactNode }) {
-	return <>{children}</>;
-}
--- lib/classNames.ts
+++
@@ -1,4 +0,0 @@
-// Usage: <div className={classNames('bg-red-500 text-white p-4', 'rounded', { 'bg-blue-500': isBlue })} />
-export default function classNames(...classes: any[]) {
-	return classes.filter(Boolean).join(' ');
-}
