f16a8e523a23b70b45b6f7a622dca47ebb490061
Colin McCulloch <colin.mcculloch@whocanfixmycar.com>
2025-09-12 01:02:40 +0100

    feat: various site updates
    replace headlessui with skeleton, add @vercel/speedinsights, update @zyzle/image-kmeans to ^3.0.0

--- src/components/Dropzone.tsx
+++ src/components/Dropzone.tsx
@@ -1,105 +1,63 @@
+import { useRef, useState } from "react";
+import { FileUpload } from "@skeletonlabs/skeleton-react";
 import {
-  type DragEventHandler,
-  useCallback,
-  useEffect,
-  useRef,
-  useState,
-} from "react";
-
-import {
-  Disclosure,
-  DisclosureButton,
-  DisclosurePanel,
-} from "@headlessui/react";
-import { ChevronUp } from "lucide-react";
-import { classNames } from "../utils/reactUtils";
+  ImagePlus as IconDropzone,
+  Paperclip as IconFile,
+  XCircle as IconRemove,
+} from "lucide-react";
 
 export type DropzoneProps = {
   droppedFile: (ibm: ImageBitmap) => void;
 };
 
 export function Dropzone(props: DropzoneProps) {
-  const { droppedFile } = props;
   const imgRef = useRef<HTMLImageElement>(null);
-  const [file, setFile] = useState<File | null>(null);
-  const [imgUrl, setImgUrl] = useState<string | null>(null);
+  const [imgVisible, setImgVisible] = useState<boolean>(false);
 
-  const handleDrop: DragEventHandler<HTMLDivElement> = useCallback(
-    async (event) => {
-      event.preventDefault();
-
-      if (
-        event.dataTransfer.items &&
-        event.dataTransfer.items[0].kind === "file"
-      ) {
-        const file = event.dataTransfer.items[0].getAsFile();
-        setFile(file as File);
-        const ibm = await createImageBitmap(file as File);
-        droppedFile(ibm);
+  const previewImage = (event: any) => {
+    const reader = new FileReader();
+    reader.onload = (e) => {
+      const image = e.target?.result;
+      if (imgRef.current && image) {
+        setImgVisible(true);
+        imgRef.current.src = image as string;
       }
-    },
-    [droppedFile, setFile]
-  );
-
-  const handleDragover: DragEventHandler<HTMLDivElement> = useCallback(
-    (event) => event.preventDefault(),
-    []
-  );
-
-  useEffect(() => {
-    if (file) {
-      const url = URL.createObjectURL(file);
-      setImgUrl(url);
-      return () => URL.revokeObjectURL(url);
+      if (props.droppedFile && event.acceptedFiles && event.acceptedFiles[0]) {
+        createImageBitmap(event.acceptedFiles[0]).then((ibm) => {
+          props.droppedFile(ibm);
+        });
+      }
+    };
+    if (event.acceptedFiles && event.acceptedFiles[0]) {
+      reader.readAsDataURL(event.acceptedFiles[0]);
     } else {
-      setImgUrl(null);
+      setImgVisible(false);
     }
-  }, [file]);
+  };
 
   return (
-    <div className="container overflow-hidden rounded-xl bg-zcomment shadow-md">
+    <>
+      <FileUpload
+        name="example"
+        accept="image/*"
+        maxFiles={1}
+        subtext="Drop an image to analyze."
+        iconInterface={<IconDropzone className="size-8" />}
+        iconFile={<IconFile className="size-4" />}
+        iconFileRemove={<IconRemove className="size-4" />}
+        onFileChange={previewImage}
+        onFileReject={console.error}
+        classes="w-full"
+      />
       <span className="not-prose">
-        {imgUrl && (
-          <img
-            ref={imgRef}
-            id="image-display"
-            src={imgUrl}
-            alt="dropped image"
-            className="h-auto w-full"
-          />
-        )}
+        <img
+          ref={imgRef}
+          id="image-display"
+          alt="dropped image"
+          className="h-auto w-1/2 mx-auto my-5"
+          hidden={!imgVisible}
+        />
       </span>
-
-      <Disclosure defaultOpen>
-        <DisclosureButton
-          className={classNames(
-            "flex w-full justify-between rounded-lg",
-            "px-4 py-2 text-left text-sm font-medium",
-            "focus:outline-none focus-visible:ring",
-            "focus-visible:ring-opacity-75"
-          )}
-        >
-          <span>Dropzone</span>
-          <ChevronUp className="group-data-open:rotate-180 h-5 w-5" />
-        </DisclosureButton>
-        <DisclosurePanel className="px-5 pt-4 pb-2 text-sm">
-          <div
-            id="drop-zone"
-            onDrop={(e) => {
-              handleDrop(e);
-            }}
-            onDragOver={handleDragover}
-            className="mx-auto cursor-pointer flex w-full max-w-lg flex-col items-center rounded-xl border-2 border-dashed p-6 text-center"
-          >
-            <h2 className="mt-4 text-xl font-medium tracking-wide">
-              Drop File
-            </h2>
-            <p className="mt-2 tracking-wide">
-              Drop an image into the box to analyze
-            </p>
-          </div>
-        </DisclosurePanel>
-      </Disclosure>
-    </div>
+    </>
   );
 }
